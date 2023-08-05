5b51b1ea736e5b7698d635376ab92249287eb38e
ChoiMP <99106482+ChoiMP@users.noreply.github.com>
2023-08-05 09:42:50 +0900

    obstacles1
    230805 0942

---
+++ Assets/Obstacles.cs
@@ -0,0 +1,50 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    [Tooltip("Ã¹¹øÂ° ÁÂÇ¥´Â ÀÚ½ÅÀÇ ÁÂÇ¥·Î ¹Ù²ñ")]
+    public Vector2[] route;
+    private int ptr;
+    public float duration = 2f;
+    enum obstacleState
+    {
+        start=1,onGoing=2,end=3
+    }
+    obstacleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        route[0] = transform.position;
+        state = obstacleState.start;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == obstacleState.start)
+        {
+            NextMove();
+        }
+        else if(state == obstacleState.end)
+        {
+            state = obstacleState.start;
+            
+        }
+    }
+
+    void NextMove()
+    {
+        state = obstacleState.onGoing;
+        transform.DOMove(route[ptr], duration)
+            .SetEase(Ease.OutCirc)
+            .OnComplete(() =>
+            {
+                state = obstacleState.end;
+                if(++ptr>=route.Length) { ptr= 0; }
+            });
+    }
+}
