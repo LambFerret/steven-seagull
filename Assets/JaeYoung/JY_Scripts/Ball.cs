480890fc0f1f25bb55f216afe7b7efb036c42ac2
Yongmin Park <yongminparks@gmail.com>
2023-08-05 19:30:39 +0900

    Ball color change to the tip as well

--- Assets/JaeYoung/JY_Scripts/Ball.cs
+++ Assets/JaeYoung/JY_Scripts/Ball.cs
@@ -32,6 +32,8 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        Color newColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        GetComponent<SpriteRenderer>().color = newColor;
+        tipSprite.color = newColor;
     }
 }
